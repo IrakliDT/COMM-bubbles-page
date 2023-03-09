1d06a0ceb3262f1aac5cf3a0d7e18f98b3534aef
Irakli DT <inhale51@gmail.com>
2023-03-09 19:58:18 +0400

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,42 @@
+// mouse cursor
+
+const blobCursor = document.querySelector('#blobCursor');
+var timeout;
+
+
+document.addEventListener('mousemove', (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+
+    blobCursor.style.top = y + "px"
+    blobCursor.style.left = x + "px"
+
+})
+
+
+//function mouse stopped
+// function mouseStopped() {
+//     blobCursor.style.display = "none"
+// }
+// timeout = setTimeout(mouseStopped, 1)
+
+
+
+// LOGIN PAGE
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "Idatia" && password === "comm") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
